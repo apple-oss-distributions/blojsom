777a94fa4277d8456581cd15e44f5445834f7bb5
Apple OSS Distributions <91980991+AppleOSSDistributions@users.noreply.github.com>
2006-06-27 17:24:55 +0000

    blojsom-63
    Imported from blojsom-63.tar.gz

--- src/com/apple/blojsom/util/AppleBlojsomVersion.java
+++ src/com/apple/blojsom/util/AppleBlojsomVersion.java
@@ -4,7 +4,7 @@
  * Copyright:  © 2004-2005 Apple Computer, Inc., all rights reserved.
  * Note:       When editing this file set PB to "Editor uses tabs/width=4".
  *
- * $Id: AppleBlojsomVersion.java,v 1.43.2.11 2006/01/19 23:15:30 johnan Exp $
+ * $Id: AppleBlojsomVersion.java,v 1.43.2.13 2006/06/27 16:10:32 johnan Exp $
  */ 
 package com.apple.blojsom.util;
 
@@ -12,11 +12,11 @@ package com.apple.blojsom.util;
  * AppleBlojsomVersion
  * 
  * @author John Anderson
- * @version $Id: AppleBlojsomVersion.java,v 1.43.2.11 2006/01/19 23:15:30 johnan Exp $
+ * @version $Id: AppleBlojsomVersion.java,v 1.43.2.13 2006/06/27 16:10:32 johnan Exp $
  */
 public class AppleBlojsomVersion {
 	
 	// Constants
-    public static final String appleBlojsomRevision = "blojsom-59";
+    public static final String appleBlojsomRevision = "blojsom-63";
     
 }
